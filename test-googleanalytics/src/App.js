2958c3b488db0e7ef36db937f813d21bd9902c44
Samantha D <deschaepmeester.samantha@gmail.com>
2023-09-07 15:41:35 +0200

    Add ga in code

--- test-googleanalytics/src/App.js
+++ test-googleanalytics/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import useAnalyticsEventTracker from './useAnalyticsEventTracker';
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-LW03WPJDEZ";
+ReactGA.initialize(TRACKING_ID);
+
 
 function App() {
+  const gaEventTracker = useAnalyticsEventTracker('Contact us');
   const handleClick = (buttonName) => {
     alert(`Vous avez cliqué sur le bouton ${buttonName}`);
   };
-  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,6 +20,12 @@ function App() {
           <button onClick={() => handleClick('Bouton 1')}>Bouton 1</button>
           <button onClick={() => handleClick('Bouton 2')}>Bouton 2</button>
           <button onClick={() => handleClick('Bouton 3')}>Bouton 3</button>
+          <div>
+            <a href="mailto:someone@example.com" onClick={()=>gaEventTracker('email')}>Ecris nous !</a>
+          </div>
+          <div>
+            <a href="#" onClick={()=>gaEventTracker('call')}>Appelle nous ;)</a>
+          </div>
         </div>
         <a
           className="App-link"
