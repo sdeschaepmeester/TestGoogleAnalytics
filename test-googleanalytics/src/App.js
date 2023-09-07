06ea01d937d19f9c84643615ddd4b7de49d1cb57
Samantha D <deschaepmeester.samantha@gmail.com>
2023-09-07 13:47:49 +0200

    add three buttons

--- test-googleanalytics/src/App.js
+++ test-googleanalytics/src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleClick = (buttonName) => {
+    alert(`Vous avez cliqué sur le bouton ${buttonName}`);
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <button onClick={() => handleClick('Bouton 1')}>Bouton 1</button>
+          <button onClick={() => handleClick('Bouton 2')}>Bouton 2</button>
+          <button onClick={() => handleClick('Bouton 3')}>Bouton 3</button>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
